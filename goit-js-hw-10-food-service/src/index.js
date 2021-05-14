158a5f86003d79113ee22a003317e4bb9e558baa
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-05-14 19:30:54 +0300

    Разметка странички

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,1 +1,14 @@
+import template from './templateCard.hbs'
+import menu from './menu.json'
+
 import './styles.css';
+
+const menuRef = document.querySelector('.js-menu');
+    console.log(menuRef)
+
+function createMenu(menu){
+    return menu.map(template).join('');
+}
+const menuMarcUp = createMenu(menu)
+console.log(menuMarcUp)
+menuRef.insertAdjacentHTML('beforeend', menuMarcUp)
