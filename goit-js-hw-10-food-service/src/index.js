f2c3bd12ecfd3018b89b6a9fd85425a0ca041a17
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-05-15 11:58:16 +0300

    finish

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -4,11 +4,51 @@ import menu from './menu.json'
 import './styles.css';
 
 const menuRef = document.querySelector('.js-menu');
-    console.log(menuRef)
+const themeToggle = document.querySelector('.theme-switch__toggle')
+const body = document.querySelector('body');
 
-function createMenu(menu){
+const createMenu =(menu) => {
     return menu.map(template).join('');
+};
+
+const menuMarcUp = createMenu(menu);
+menuRef.insertAdjacentHTML('beforeend', menuMarcUp);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const reload = () => {
+    const check = localStorage.getItem('theme');
+    if (check === Theme.DARK) {
+        themeToggle.checked = true
+        body.classList.add(Theme.DARK);
+    }
+};
+
+body.classList.add(reload());
+
+const saveTheme = () => {
+    if (themeToggle.checked) {
+        console.log(themeToggle.checked)
+        localStorage.setItem('theme', Theme.DARK)
+    }
+    else {
+        localStorage.removeItem('theme')
+    }
 }
-const menuMarcUp = createMenu(menu)
-console.log(menuMarcUp)
-menuRef.insertAdjacentHTML('beforeend', menuMarcUp)
+
+const changeTheme = (click) => {
+    if (themeToggle.checked) {
+        // body.classList.remove(Theme.LIGHT);
+        body.classList.add(Theme.DARK);
+        saveTheme()
+    }
+    else {
+        // body.classList.add(Theme.LIGHT);
+        body.classList.remove(Theme.DARK);
+        saveTheme()
+    }        
+}
+themeToggle.addEventListener('click', changeTheme);
