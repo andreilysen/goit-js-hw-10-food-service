f051d17d4bbd26b4b8bf6ce853534e42dc050d00
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-05-16 22:43:47 +0300

    формат Pretier

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,14 +1,14 @@
-import template from './templateCard.hbs'
-import menu from './menu.json'
+import template from './templateCard.hbs';
+import menu from './menu.json';
 
 import './styles.css';
 
 const menuRef = document.querySelector('.js-menu');
-const themeToggle = document.querySelector('.theme-switch__toggle')
+const themeToggle = document.querySelector('.theme-switch__toggle');
 const body = document.querySelector('body');
 
-const createMenu =(menu) => {
-    return menu.map(template).join('');
+const createMenu = menu => {
+  return menu.map(template).join('');
 };
 
 const menuMarcUp = createMenu(menu);
@@ -19,32 +19,29 @@ const Theme = {
   DARK: 'dark-theme',
 };
 const reload = () => {
-    const check = localStorage.getItem('theme');
-    if (check === Theme.DARK) {
-        themeToggle.checked = true
-        body.classList.add(Theme.DARK);
-    }
-    else {
-        body.classList.add(Theme.LIGHT)
-
-    }
+  const check = localStorage.getItem('theme');
+  if (check === Theme.DARK) {
+    themeToggle.checked = true;
+    body.classList.add(Theme.DARK);
+  } else {
+    body.classList.add(Theme.LIGHT);
+  }
 };
 
 reload();
 
 const replaceTheme = (oldTheme, newTheme) => {
-    body.classList.remove(oldTheme);
-    body.classList.add(newTheme);
-    localStorage.setItem('theme', newTheme)
-}
+  body.classList.remove(oldTheme);
+  body.classList.add(newTheme);
+  localStorage.setItem('theme', newTheme);
+};
 
-const changeTheme = (click) => {
-    if (themeToggle.checked) {
-        replaceTheme(Theme.LIGHT, Theme.DARK)
-    }
-    else {
-        replaceTheme(Theme.DARK, Theme.LIGHT)
-    }        
-}
+const changeTheme = click => {
+  if (themeToggle.checked) {
+    replaceTheme(Theme.LIGHT, Theme.DARK);
+  } else {
+    replaceTheme(Theme.DARK, Theme.LIGHT);
+  }
+};
 
 themeToggle.addEventListener('click', changeTheme);
